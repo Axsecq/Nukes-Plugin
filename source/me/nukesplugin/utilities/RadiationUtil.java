8faa591ad0cd5784cdef1508412eade2fabfbd95
Axsecq <117029366+axsecq@users.noreply.github.com>
2022-11-02 20:34:24 +1000

    Add files via upload

--- source/me/nukesplugin/utilities/RadiationUtil.java
+++ source/me/nukesplugin/utilities/RadiationUtil.java
@@ -1,5 +1,10 @@
 package me.nukesplugin.utilities;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
 import me.nukesplugin.NukesPlugin;
 
 public class RadiationUtil {
@@ -23,5 +28,18 @@ public class RadiationUtil {
 		this.plugin.config.getConfig().set("radioactive", state);
 		this.plugin.config.save();
 		this.radioactive = state;
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void giveRadiation(Player player) {
+		if (isRadioactive() && !player.hasPotionEffect(PotionEffectType.REGENERATION)) {
+			
+	    	player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 100, 2, false, false));
+	    	player.addPotionEffect(new PotionEffect(PotionEffectType.POISON,    100, 0, false, false));
+	    	player.addPotionEffect(new PotionEffect(PotionEffectType.WITHER,    100, 0, false, false));
+	    	
+	    	player.sendTitle("⚠︎", "You are poisoned with radiation.");
+	    	
+		};
 	}
 }
